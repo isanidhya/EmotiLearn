16d96896902e0c19d58028190ea7456e6c70c38a
Sanidhya Sahu <sanidhyasahu1120@gmail.com>
2025-12-09 19:35:46 +0000

    Here is credentials const firebaseConfig = {   apiKey: "AIzaSyDjfGaZ3CRt

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,10 +4,10 @@
 // For more information on how to secure your Firebase project, see:
 // https://firebase.google.com/docs/projects/learn-more#config-files-objects
 export const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "example.firebaseapp.com",
-  projectId: "example",
-  storageBucket: "example.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:xxxxxxxxxxxxxxxxxxxxxx",
+  apiKey: "AIzaSyDjfGaZ3CRtmid9NN_y4NgODE2vb4tiSrI",
+  authDomain: "studio-144842297-c34c4.firebaseapp.com",
+  projectId: "studio-144842297-c34c4",
+  storageBucket: "studio-144842297-c34c4.appspot.com",
+  messagingSenderId: "585810042214",
+  appId: "1:585810042214:web:e19fab2a11610a7c353ca7",
 };
